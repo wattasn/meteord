7371d0da179d5cdbe9346b8979237b08e8470604
Watanabe <hrk@watta.jp>
2016-05-22 15:20:38 +0900

    libbz2-dev

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential python git
+apt-get install -y curl libbz2-dev build-essential python git
 apt-get install -y supervisor
